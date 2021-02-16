e36d4b0307a46efe6a4242c36a78df32112c4017
Benjamin <1234hteimaj4321@gmail.com>
2021-02-15 17:29:49 -0700

    Added nav to home jic

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import { Box, Typography } from "@material-ui/core";
+import PortfolioNav from "../components/portfolio/PortfolioNav";
 
 export default function Home() {
   return (
     <>
       <Box>
         <Typography variant="h1">Home</Typography>
+        <PortfolioNav />
       </Box>
     </>
   );
