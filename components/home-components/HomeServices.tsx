d071e0dbec6ab9f824569d70fcbb3d8ba96680b9
Benjamin <1234hteimaj4321@gmail.com>
2021-02-22 17:06:47 -0700

    Finished mobile for services section

--- components/home-components/HomeServices.tsx
+++ components/home-components/HomeServices.tsx
@@ -5,86 +5,90 @@ import {
   Divider,
   Grid,
   Typography,
+  makeStyles,
 } from "@material-ui/core";
+import { ServiceContainer } from "./ServiceContainer";
+
+const useStyles = makeStyles((theme) => ({
+  mainContainer: {
+    padding: "1rem 0",
+  },
+  secondaryContainer: {
+    padding: "1.3rem 0",
+  },
+  mainTitle: {
+    padding: ".3rem 0",
+    marginTop: "1rem",
+  },
+  secondaryTitle: {
+    background: theme.palette.secondary.main,
+    borderRadius: ".1rem",
+    padding: "0 .2rem",
+    color: theme.palette.secondary.contrastText,
+  },
+}));
 
 export const HomeServices = () => {
+  const {
+    mainTitle,
+    secondaryTitle,
+    mainContainer,
+    secondaryContainer,
+  } = useStyles();
   return (
     <Container>
-      <Typography variant="h2">Services</Typography>
-      <Grid container>
+      <Typography className={mainTitle} variant="h2">
+        Services
+      </Typography>
+      <Grid className={mainContainer} container>
         <Grid item>
-          <Box>
-            <Typography variant="h4" component="h3">
+          <Box className={secondaryContainer}>
+            <Typography className={secondaryTitle} variant="h4" component="h3">
               Websites
             </Typography>
-            <Typography variant="h5" component="h4">
-              Next.js
-            </Typography>
-            <Divider />
-            <Typography variant="body1">
+            <ServiceContainer title="Next.js" link="/next-pricing">
               If you want an extremely customizable site where you can add
               detail to every little pixle and light speed performance with SEO
               in mind then using Next.js is what will get you there.
-            </Typography>
-            <Button color="secondary" fullWidth variant="outlined">
-              Pricing
-            </Button>
-            <Typography variant="h5" component="h4">
-              Wordpress
-            </Typography>
-            <Divider />
-            <Typography variant="body1">
+            </ServiceContainer>
+            <ServiceContainer title="Wordpress" link="/wordpress-pricing">
               Wordpress allows for very quick production and easy editability.
               Speed is not it's number one priority nor is no touch maintenance
               but it is one of the most used website builders out there.
-            </Typography>
-            <Button color="secondary" fullWidth variant="outlined">
-              Pricing
-            </Button>
+            </ServiceContainer>
           </Box>
         </Grid>
         <Grid item>
-          <Box>
-            <Typography variant="h4" component="h3">
+          <Box className={secondaryContainer}>
+            <Typography className={secondaryTitle} variant="h4" component="h3">
               Apps
             </Typography>
-            <Typography variant="h5" component="h4">
-              React.js
-            </Typography>
-            <Divider />
-            <Typography variant="body1">
+            <ServiceContainer title="React.js" link="/react-pricing">
               If you're not worried about SEO and being listed as a website then
               React.js is the perfect framework.
-            </Typography>
-            <Button color="secondary" fullWidth variant="outlined">
-              Pricing
-            </Button>
-            <Typography variant="h5" component="h4">
-              React Native (Mobile specific)
-            </Typography>
-            <Divider />
-            <Typography variant="body1">
+            </ServiceContainer>
+            <ServiceContainer
+              title="React Native (Mobile)"
+              link="/react-native-pricing"
+            >
               Do you need an app that just runs on Android or IPhone? React
               Native is an amazing framework built just for that!
-            </Typography>
-            <Button color="secondary" fullWidth variant="outlined">
-              Pricing
-            </Button>
+            </ServiceContainer>
           </Box>
         </Grid>
         <Grid item>
-          <Box>
-            <Typography variant="h4" component="h3">
+          <Box className={secondaryContainer}>
+            <Typography className={secondaryTitle} variant="h4" component="h3">
               Mentoring
             </Typography>
-            <Typography variant="body1">
+            <ServiceContainer
+              title="Web Development"
+              link="/mentorship-pricing"
+            >
               Since working with HTML, CSS, JavaScript and React is what I do
               for a living, I also offer mentorship services in any of these
               areas!
-            </Typography>
-            <Button color="secondary" fullWidth variant="outlined">
-              Pricing
-            </Button>
+            </ServiceContainer>
           </Box>
         </Grid>
       </Grid>
---
+++ components/home-components/ServiceContainer.tsx
@@ -0,0 +1,50 @@
+import Link from "next/link";
+import {
+  Box,
+  Button,
+  Divider,
+  makeStyles,
+  Typography,
+} from "@material-ui/core";
+
+interface IServiceContainer {
+  title: string;
+  children: string;
+  link: string;
+}
+
+const useStyles = makeStyles({
+  root: {
+    padding: "1rem 0",
+  },
+  titleStyle: {
+    padding: ".8rem 0",
+  },
+  textStyle: {
+    padding: ".6rem 0",
+  },
+});
+
+export const ServiceContainer = ({
+  title,
+  link,
+  children,
+}: IServiceContainer) => {
+  const { root, titleStyle, textStyle } = useStyles();
+  return (
+    <Box className={root}>
+      <Typography className={titleStyle} variant="h5" component="h4">
+        {title}
+      </Typography>
+      <Divider />
+      <Typography className={textStyle} variant="body1">
+        {children}
+      </Typography>
+      <Link href={link}>
+        <Button color="secondary" fullWidth variant="outlined">
+          Pricing
+        </Button>
+      </Link>
+    </Box>
+  );
+};
